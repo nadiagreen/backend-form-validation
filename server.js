46731316b965e41bfb9a47f0b573da4da944c5c6
nadiagreen <nadiagreen>
2019-08-18 17:22:42 +0300

    backend and frontent

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+    res.send('Hello, Shurik!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+ 
